43cdf839ce7e09891da84c8d667e39e9ff8d322f
PatrickBoynton <jpboynton3@gmail.com>
2021-02-12 16:06:32 -0500

    In class refactor.

--- src/App.js
+++ src/App.js
@@ -4,40 +4,52 @@ import {Component} from "react";
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+        // isToggled: false,
+        bookmarks: [],
+        filter: null,
+    };
+
+    // this.handleClick = this.handleClick.bind(this);
   }
 
   componentDidMount() {
-    const urls = [
-        "https://google.com",
-        "https://facebook.com",
-        "https://wikipedia.org",
-        "https://espn.com",
-        "https://bing.com"
-    ]
+      const bookmarks = [
+          {tag: "search engine", url: "https://google.com"},
+          {tag: "social media", url: "https://facebook.com"},
+          {tag: "reference", url: "https://wikipedia.org"},
+          {tag: "social media", url: "https://instagram.com"},
+      ]
 
-    const tags = [
-        "search engine",
-        "social media",
-        "reference",
-        "sports",
-        "search engine"
-    ]
-
-    this.setState({urls, tags});
+      this.setState({bookmarks});
   }
 
-  render() {
-    const tags = this.state.tags?.map((tag, index) =>(
-        <li>
-            <button>{tag}</button>
-        </li>
-    ))
+  // handleClick(index) {
+  //     this.setState({urls: this.state.bookmarks[index].urls.map(url => url)});
+  //     console.log(this.state.urls)
+  // }
 
+  render() {
+
+     const tags = this.state.bookmarks.map(bookmark => bookmark.tag);
+     const uniqueTags = Array.from(new Set(tags));
+     const tagsHTML = uniqueTags.map((tag, index) => (
+         <button key={index} onClick={() => this.setState({filter: tag})}>{tag}</button>
+     ));
+
+    const bookmarks = this.state.bookmarks
+        .filter(bookmark => this.state.filter ? bookmark.tag === this.state.filter : 9)
+        .map((bookmark, index) =>
+        <li key={index}>
+            <p>{bookmark.url}</p>
+        </li>);
     return (
         <div className="App">
+            <nav>
+                {tagsHTML}
+            </nav>
             <ul>
-                {tags}
+                {bookmarks}
             </ul>
         </div>
     );
